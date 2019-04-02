da16ccd98ffe7b0f5ad80e831176752594a8ded6
ansen <shen.an89@gmail.com>
2019-04-02 22:54:10 +0800

    1. config文件默认使用svnserver配置，添加服务器ip配置项

--- config.py
+++ config.py
@@ -8,18 +8,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 #本系统使用的端口号
+SERVER_IP = '127.0.0.1'
 SERVER_PORT = 358
 
-#SVN配置库存放的根目录，以\\结尾
-REPOS_DIRS='d:\SVNMaster\Repos\\'
-REPOS_BASE_URL='http://127.0.0.1/svn/'
+#SVN配置库存放的根目录
+REPOS_DIRS='/data/svndata/'
+REPOS_BASE_URL='svn://127.0.0.1/'
 
 #SVN认证相关配置
 #支持httpd（即Apache集成方式）或svnserver
-SVN_AUTH_MODE = 'httpd'
+SVN_AUTH_MODE = 'svnserver'
 #Apache集成方式下的权限（authz）和htpasswd（用户&密码）文件位置
-SVN_HTTPD_AUTHZ = 'd:\SVNMaster\Repos\\authz'
-SVN_HTTPD_USERS = 'd:\SVNMaster\Repos\\htpasswd'
+SVN_HTTPD_AUTHZ = ''
+SVN_HTTPD_USERS = ''
 #HTTPD_HTPASSWD_FILE = 'd:\SVNMaster\Apache\bin\htpasswd.exe'
 #SVN Server 服务方式时，用户/权限文件默认保存在每个库的conf文件夹下
 
@@ -29,7 +30,7 @@ SVN_HTTPD_USERS = 'd:\SVNMaster\Repos\\htpasswd'
 
 #以下信息不要修改-------------以下信息不要修改-------------以下信息不要修改-------------以下信息不要修改-------------
 #数据库配置
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '.\mos\dbs\sm_data.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '\mos\dbs\sm_data.db')
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, '.\mos\dbs\db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
--- server.py
+++ server.py
@@ -4,11 +4,11 @@ __author__="EIXXIE"
 
 import sys,os
 from mos import app
-from config import SERVER_PORT
+from config import SERVER_IP,SERVER_PORT
 
 #将mos文件夹添加到系统变量中，以便import
 sys.path.append(os.path.dirname(app.root_path))
 #启动MOS
-app.run(host='0.0.0.0',port=SERVER_PORT,debug=True)
+app.run(host=SERVER_IP,port=SERVER_PORT,debug=True)
 
 #print(sys.path)
